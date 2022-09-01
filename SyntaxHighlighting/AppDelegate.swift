b57707e8f5b7dc0ce66a6f02472f30df8d197e13
Cleber Neitzke <clneitzke@gmail.com>
2022-09-01 20:59:39 -0300

    Initial Commit

---
+++ SyntaxHighlighting/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  SyntaxHighlighting
+//
+//  Created by Cleber Neitzke on 01/09/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
